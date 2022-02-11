67a7f9e820fc3ddb71227c05eca18115a9b48f8b
Mykola Veremchuk <36420699+mykola-vrmchk@users.noreply.github.com>
2022-02-11 18:55:17 +0200

    Update drawing.py
    Fixed error in color_gradient for the case of applying the circular mask

--- moviepy/video/tools/drawing.py
+++ moviepy/video/tools/drawing.py
@@ -140,7 +140,7 @@ def color_gradient(size,p1,p2=None,vector=None, r=None, col1=0,col2=1.0,
     if vector is None and p2:
         p2 = np.array(p2[::-1])
         vector = p2-p1
-    else:
+    elif vector is not None:
         vector = np.array(vector[::-1])
         p2 = p1 + vector
     
